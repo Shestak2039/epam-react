1a302b250f4e08fb108132671401d36e7fff6597
uladzislau shastakou <uladzislau_shastakou@epam.com>
2021-03-04 22:00:02 +0300

    feat: setup typescript for project

---
+++ webpack.config.ts
@@ -0,0 +1,77 @@
+import path from 'path';
+import webpack from 'webpack';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+
+const config: webpack.Configuration = {
+    entry: path.join(__dirname, 'src/index.tsx'),
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './build')
+    },
+    mode: process.env.NODE_ENV as 'production' | 'development',
+    optimization: {
+        minimize: process.env.NODE_ENV === 'production',
+        minimizer: [new TerserPlugin()]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|js)x?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-react',
+                            '@babel/preset-typescript'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: `./public/index.html`
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new CleanWebpackPlugin(),
+        new ForkTsCheckerWebpackPlugin({
+            async: false,
+            eslint: {
+                files: './src/**/*.{ts,tsx,js,jsx}'
+            }
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'build'),
+        compress: true,
+        port: 3000
+    }
+};
+
+export default config;
