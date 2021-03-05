abc52ad9a2353619623d23b6420491c27372a840
uladzislau shastakou <uladzislau_shastakou@epam.com>
2021-03-05 14:10:23 +0300

    feat: add ErrorBoundary

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,20 @@ import Wrapper from './components/Wrapper/Wrapper';
 import Main from './components/Main/Main';
 import SortingFilms from './components/SortingFilms/SortingFilms';
 import FilmsList from './components/FilmsList/FilmsList';
+import Footer from './components/Footer/Footer';
+import ErrorBoundary from './components/common/ErrorBoundary/ErrorBoundary';
 
 const App: React.FunctionComponent = () => (
-    <Wrapper>
-        <Header />
-        <Main>
-            <SortingFilms />
-            <FilmsList />
-        </Main>
-    </Wrapper>
+    <ErrorBoundary>
+        <Wrapper>
+            <Header />
+            <Main>
+                <SortingFilms />
+                <FilmsList />
+            </Main>
+            <Footer />
+        </Wrapper>
+    </ErrorBoundary>
 );
 
 export default App;
